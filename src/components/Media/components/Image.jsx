9db2f719f3ff1756edb1f1fdc39daeb3cdcf3a6e
stephanemonier <smonier@gmail.com>
2021-06-09 17:46:21 +0200

    fixed jContent image display

--- src/components/Media/components/Image.jsx
+++ src/components/Media/components/Image.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 const Image = ({path,alt}) =>{
-    const {files_endpoint} = process.env.REACT_APP_JCONTENT_FILES_ENDPOINT;
     return(
         <img className="d-block w-100"
-             src={`${files_endpoint}${encodeURI(path)}`}
+             src={process.env.REACT_APP_JCONTENT_FILES_ENDPOINT+`${encodeURI(path)}`}
              alt={alt}/>
     )
 }
